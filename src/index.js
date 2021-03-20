60459e8023a45c8c4f2a618f5b1671d1b2708425
DavidRMorphew <morphewd@umich.edu>
2021-03-20 13:54:55 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const formForSubmit = document.getElementById('create-task-form');
+  const ul = document.getElementById('tasks');
+  
+  formForSubmit.addEventListener('submit', function(event){
+    const li = document.createElement('li');
+    li.innerText = document.getElementById('new-task-description').value;
+    ul.append(li);
+    event.preventDefault();
+})});
 
-const formForSubmit = document.getElementById('create-task-form');
-const ul = document.getElementById('tasks');
-
-formForSubmit.addEventListener('submit', function(event){
-  const li = document.createElement('li');
-  li.innerText = document.getElementById('new-task-description').value;
-  ul.append(li);
-  event.preventDefault();
-})
