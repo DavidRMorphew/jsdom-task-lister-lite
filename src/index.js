4704c36c3bf52795597b9be9003d87bae179c3bf
DavidRMorphew <morphewd@umich.edu>
2021-03-20 19:48:26 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   const formForSubmit = document.getElementById('create-task-form');
   const ul = document.getElementById('tasks');
-  
+  const inputField = document.getElementById('new-task-description')
+
   formForSubmit.addEventListener('submit', function(event){
     const li = document.createElement('li');
-    li.innerText = document.getElementById('new-task-description').value;
+    li.innerText = inputField.value;
     ul.append(li);
+    inputField.value = "";
     event.preventDefault();
-})});
+  })
+});
 
